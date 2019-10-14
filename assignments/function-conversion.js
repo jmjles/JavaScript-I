751b0d3d54b57a4236d7269da8a58266ce94cb9a
jmjles <jmjles@gmail.com>
2019-10-14 15:53:45 -0700

    MVP Completed

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,30 +1,29 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
+console.log('########## Functions ##########');
+let myFunction = () => console.log("Function was invoked!");
+myFunction();
 
-// let myFunction = function () {
-// console.log("Function was invoked!");
-// };
-// myFunction();
+document.getElementById('fChallenge1').innerHTML = `let myFunction = () => console.log("Function was invoked!");`;
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
-// anotherFunction("Example");
+let anotherFunction = (param) => console.log(param);
+anotherFunction("Example");
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
+document.getElementById('fChallenge2').innerHTML = `let anotherFunction = (param) => console.log(param);`;
 
-// let subtract = function (param1, param2) {
-//   return param1 - param2;
-// };
-// subtract(1,2);
+let add = (param1, param2) => console.log(param1 + param2);
+add(1,2);
 
+document.getElementById('fChallenge3').innerHTML = `let add = (param1, param2) => console.log(param1 + param2);`;
+
+let subtract =(param1, param2) => console.log(param1 - param2);
+subtract(1,2);
+
+document.getElementById('fChallenge4').innerHTML = `let subtract =(param1, param2) => console.log(param1 - param2);`;
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num) => num * 3);
+console.log(triple);
+
+document.getElementById('sChallenge2').innerHTML = `const triple = exampleArray.map((num) => num * 3);`;
